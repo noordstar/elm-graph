e8f7bc45b5f3480fde9eb98134363c62296da1ef
Bram <bram@noordstar.me>
2024-06-11 11:39:06 +0200

    Finish basic Graph API

--- src/Graph.elm
+++ src/Graph.elm
@@ -1,5 +1,18 @@
-module Graph exposing (Graph, Vertex, vLabel, Edge, eLabel, origin, destination)
-{-| # Graph
+module Graph exposing
+    ( Graph
+    , Vertex, vLabel
+    , Edge, eLabel, origin, destination
+    , empty, singleton, insertV, insertE, updateE, removeE, removeV
+    , isEmpty, getE, sizeE, sizeV
+    , v, e
+    , mapV, mapE
+    , pointsTo
+    )
+
+{-|
+
+
+# Graph
 
 The Graph type helps define relational data.
 
@@ -16,79 +29,209 @@ direction and a label. There can not be more than two edges between two vertices
 
 @docs Edge, eLabel, origin, destination
 
-## Create
 
-@docs empty
+## Build
+
+@docs empty, singleton, insertV, insertE, updateE, removeE, removeV
+
+
+## Query
+
+@docs isEmpty, getE, sizeE, sizeV
+
+
+## Lists
+
+@docs v, e
+
+
+## Transform
+
+@docs mapV, mapE
+
+
+## Traverse
+
+@docs pointsTo
+
 -}
 
-import Internal
-import Set exposing (Set)
 import FastDict as Dict
+import Internal
+
 
 {-| An edge is a relation between two vertices. For example, one person can have
 a relationship with another person - and you can use a custom `edge` type to
 reveal more about the _type_ of relationship.
 -}
-type Edge vertex edge =
-    Edge { destination : Vertex vertex, edge : edge, origin : Vertex vertex, ptr : Int }
+type Edge vertex edge
+    = Edge { destination : Vertex vertex, edge : edge, origin : Vertex vertex }
+
 
 {-| A graph is a large data type containing various data points that are linked
 to each other.
 -}
-type Graph vertex edge =
-    Graph (Internal.Graph vertex edge)
+type Graph vertex edge
+    = Graph (Internal.Graph vertex edge)
+
 
 {-| A Vertex is a data point within the graph. You can store various bits of
 content in it.
 -}
-type Vertex vertex =
-    Vertex { ptr : Int, vertex : vertex }
+type Vertex vertex
+    = Vertex { ptr : Int, vertex : vertex }
+
 
 {-| Get the destination that an edge points to.
 -}
 destination : Edge vertex edge -> Vertex vertex
-destination (Edge e) = e.destination
+destination (Edge ed) =
+    ed.destination
+
+
+{-| Get a list of all edges
+-}
+e : Graph vertex edge -> List (Edge vertex edge)
+e (Graph g) =
+    Internal.toList g
+        |> List.concatMap
+            (\( key, vx1 ) ->
+                vx1.source
+                    |> Dict.toList
+                    |> List.filterMap
+                        (\( dest, edge ) ->
+                            case Internal.getV dest g of
+                                Just vx2 ->
+                                    Just
+                                        (Edge
+                                            { destination = Vertex { ptr = dest, vertex = vx2.content }
+                                            , edge = edge
+                                            , origin = Vertex { ptr = key, vertex = vx1.content }
+                                            }
+                                        )
+
+                                _ ->
+                                    Nothing
+                        )
+            )
+
 
 {-| Finds the label of an edge.
 -}
 eLabel : Edge vertex edge -> edge
-eLabel (Edge e) = e.edge
+eLabel (Edge ed) =
+    ed.edge
+
 
 {-| A new and empty graph containing no vertices.
 -}
 empty : Graph vertex edge
-empty = Graph Internal.empty
+empty =
+    Graph Internal.empty
+
+
+{-| Get an edge between two vertices, if it exists.
+-}
+getE : { from : Vertex vertex, to : Vertex vertex } -> Graph vertex edge -> Maybe (Edge vertex edge)
+getE data (Graph g) =
+    case ( data.from, data.to ) of
+        ( Vertex f, Vertex t ) ->
+            Internal.getE { from = f.ptr, to = t.ptr } g
+                |> Maybe.map
+                    (\ed ->
+                        Edge
+                            { destination = data.to
+                            , edge = ed
+                            , origin = data.from
+                            }
+                    )
+
+
+{-| Insert a new edge into the graph. If the edge already exists, it is
+overwritten.
+-}
+insertE : { content : edge, from : Vertex vertex, to : Vertex vertex } -> Graph vertex edge -> Graph vertex edge
+insertE data (Graph g) =
+    case ( data.from, data.to ) of
+        ( Vertex f, Vertex t ) ->
+            Graph
+                (Internal.insertE
+                    { content = data.content, from = f.ptr, to = t.ptr }
+                    g
+                )
+
+
+{-| Checks whether a graph is empty.
+-}
+isEmpty : Graph vertex edge -> Bool
+isEmpty (Graph g) =
+    Internal.isEmpty g
+
 
 {-| Insert a new vertex into the graph.
 -}
-insertV : vertex -> Graph vertex edge -> (Vertex vertex, Graph vertex edge)
-insertV v (Graph g) =
-    case Internal.insertV v g of
+insertV : vertex -> Graph vertex edge -> ( Vertex vertex, Graph vertex edge )
+insertV vx (Graph g) =
+    case Internal.insertV vx g of
         ( ptr, newG ) ->
-            ( Vertex { ptr = ptr, vertex = v }, Graph newG )
+            ( Vertex { ptr = ptr, vertex = vx }, Graph newG )
+
+
+{-| Map all edge labels to a new value. Note that this function is completely
+blind to the location of each edge.
+-}
+mapE : (edge1 -> edge2) -> Graph vertex edge1 -> Graph vertex edge2
+mapE f (Graph g) =
+    Graph (Internal.mapE f g)
+
+
+
+-- TODO: Give liberty to view environment
+
+
+{-| Map all vertex labels to a new value. This function cannot change the
+structure of the graph, but it can use the structure of the graph to remap the
+labels.
+-}
+mapV : (Vertex vertex1 -> vertex2) -> Graph vertex1 edge -> Graph vertex2 edge
+mapV f (Graph g) =
+    Graph (Internal.mapV (\i vx -> f (Vertex { ptr = i, vertex = vx })) g)
+
 
 {-| Get the vertex that an edge starts from.
 -}
 origin : Edge vertex edge -> Vertex vertex
-origin (Edge e) = e.origin
+origin (Edge ed) =
+    ed.origin
+
 
 {-| Get all the edges that start from a given vertex.
 -}
-pointsTo : Vertex vertex -> Graph vertex edge -> List (edge, Vertex vertex)
+pointsTo : Vertex vertex -> Graph vertex edge -> List ( edge, Vertex vertex )
 pointsTo (Vertex { ptr }) (Graph g) =
     Internal.getV ptr g
         |> Maybe.map .source
         |> Maybe.withDefault Dict.empty
         |> Dict.toList
         |> List.filterMap
-            (\(i, e) ->
+            (\( i, ed ) ->
                 g
                     |> Internal.getV i
                     |> Maybe.map
                         (\vx ->
-                            ( e, Vertex { ptr = i, vertex = vx.content })
+                            ( ed, Vertex { ptr = i, vertex = vx.content } )
                         )
             )
+
+
+{-| Remove an edge from the graph.
+-}
+removeE : { from : Vertex vertex, to : Vertex vertex } -> Graph vertex edge -> Graph vertex edge
+removeE data (Graph g) =
+    case ( data.from, data.to ) of
+        ( Vertex f, Vertex t ) ->
+            Graph (Internal.removeE { from = f.ptr, to = t.ptr } g)
+
 
 {-| Remove a vertex from the graph.
 -}
@@ -96,12 +239,55 @@ removeV : Vertex vertex -> Graph vertex edge -> Graph vertex edge
 removeV (Vertex { ptr }) (Graph g) =
     Graph (Internal.removeV ptr g)
 
+
 {-| Create a graph with a single vertex in it.
 -}
 singleton : vertex -> ( Vertex vertex, Graph vertex edge )
-singleton v = insertV v empty
+singleton vx =
+    insertV vx empty
+
+
+{-| Determines the number of edges in a graph.
+-}
+sizeE : Graph vertex edge -> Int
+sizeE (Graph g) =
+    Internal.sizeE g
+
+
+{-| Determines the number of vertices in a graph.
+-}
+sizeV : Graph vertex edge -> Int
+sizeV (Graph g) =
+    Internal.sizeV g
+
+
+{-| Check whether an edge already exists, and update it according to your needs.
+-}
+updateE : { from : Vertex vertex, to : Vertex vertex } -> (Maybe edge -> Maybe edge) -> Graph vertex edge -> Graph vertex edge
+updateE data func (Graph g) =
+    case ( data.from, data.to ) of
+        ( Vertex f, Vertex t ) ->
+            case func (Internal.getE { from = f.ptr, to = t.ptr } g) of
+                Just ed ->
+                    insertE { content = ed, from = data.from, to = data.to } (Graph g)
+
+                Nothing ->
+                    removeE { from = data.from, to = data.to } (Graph g)
+
+
+{-| Get a list of all vertices.
+-}
+v : Graph vertex edge -> List (Vertex vertex)
+v (Graph g) =
+    Internal.toList g
+        |> List.map
+            (\( key, vx ) ->
+                Vertex { vertex = vx.content, ptr = key }
+            )
+
 
 {-| Finds the label of a vertex.
 -}
 vLabel : Vertex vertex -> vertex
-vLabel (Vertex v) = v.vertex
+vLabel (Vertex vx) =
+    vx.vertex
--- src/Internal.elm
+++ src/Internal.elm
@@ -1,16 +1,21 @@
 module Internal exposing (..)
 
-import Iddict exposing (Iddict)
 import FastDict as Dict exposing (Dict)
+import Iddict exposing (Iddict)
 import Set exposing (Set)
+
 
 {-| Graph's edge.
 -}
-type alias Edge edge = { content : edge, from : Int, to : Int }
+type alias Edge edge =
+    { content : edge, from : Int, to : Int }
+
 
 {-| Elm representation of a graph.
 -}
-type alias Graph vertex edge = Iddict (Vertex vertex edge)
+type alias Graph vertex edge =
+    Iddict (Vertex vertex edge)
+
 
 {-| Graph's vertex.
 -}
@@ -20,11 +25,13 @@ type alias Vertex vertex edge =
     , source : Dict Int edge
     }
 
+
 {-| Add an edge, indicating that the edge ends here.
 -}
 addEdgePointerToSink : Int -> Vertex vertex edge -> Vertex vertex edge
 addEdgePointerToSink key v =
     { v | sink = Set.insert key v.sink }
+
 
 {-| Add an edge, indicating that the edge starts here.
 -}
@@ -32,10 +39,13 @@ addEdgePointerToSource : Int -> edge -> Vertex vertex edge -> Vertex vertex edge
 addEdgePointerToSource key e v =
     { v | source = Dict.insert key e v.source }
 
+
 {-| An empty graph containing no vertices or edges.
 -}
 empty : Graph vertex edge
-empty = Iddict.empty
+empty =
+    Iddict.empty
+
 
 {-| An empty vertex containing no metadata other than the provided information.
 -}
@@ -46,6 +56,7 @@ emptyV v =
     , source = Dict.empty
     }
 
+
 {-| Get an edge based on its adjacent vertices.
 -}
 getE : { from : Int, to : Int } -> Graph vertex edge -> Maybe edge
@@ -55,36 +66,100 @@ getE data g =
         |> Maybe.map .source
         |> Maybe.andThen (Dict.get data.to)
 
+
 {-| Get a vertex by its id.
 -}
 getV : Int -> Graph vertex edge -> Maybe (Vertex vertex edge)
 getV =
     Iddict.get
 
+
+isEmpty : Graph vertex edge -> Bool
+isEmpty =
+    Iddict.isEmpty
+
+
 {-| Insert a new edge into the graph - if both ends exist.
 -}
-insertE : { content : edge, from : Int, to : Int } -> Graph vertex edge -> Graph vertex edge
+insertE : Edge edge -> Graph vertex edge -> Graph vertex edge
 insertE e g =
-    case (Iddict.get e.from g, Iddict.get e.to g) of
+    case ( Iddict.get e.from g, Iddict.get e.to g ) of
         ( Just v1, Just v2 ) ->
             g
                 |> Iddict.update e.from
                     (always <| Just <| addEdgePointerToSource e.to e.content v1)
                 |> Iddict.update e.to
                     (always <| Just <| addEdgePointerToSink e.from v2)
-        
+
         _ ->
             g
 
+
 {-| Insert a new vertex into the graph.
 -}
-insertV : vertex -> Graph vertex edge -> (Int, Graph vertex edge)
+insertV : vertex -> Graph vertex edge -> ( Int, Graph vertex edge )
 insertV v g =
     Iddict.insert (emptyV v) g
+
+
+mapE : (edge1 -> edge2) -> Graph vertex edge1 -> Graph vertex edge2
+mapE f g =
+    Iddict.map
+        (\_ v ->
+            { content = v.content
+            , sink = v.sink
+            , source = Dict.map (always f) v.source
+            }
+        )
+        g
+
+
+mapV : (Int -> vertex1 -> vertex2) -> Graph vertex1 edge -> Graph vertex2 edge
+mapV f g =
+    Iddict.map (\i v -> { content = f i v.content, sink = v.sink, source = v.source }) g
+
+
+{-| Remove an edge from the graph.
+-}
+removeE : { from : Int, to : Int } -> Graph vertex edge -> Graph vertex edge
+removeE data g =
+    g
+        |> Iddict.update data.from
+            (Maybe.map (\v -> { v | source = Dict.remove data.to v.source }))
+        |> Iddict.update data.to
+            (Maybe.map (\v -> { v | sink = Set.remove data.from v.sink }))
+
 
 {-| Remove a vertex from the graph, including all edges that connect the vertex
 to its neighbours.
 -}
 removeV : Int -> Graph vertex edge -> Graph vertex edge
-removeV =
-    Iddict.remove
+removeV key g =
+    case Iddict.get key g of
+        Just vx ->
+            g
+                |> Set.foldl
+                    (\from -> removeE { from = from, to = key })
+                |> (|>) vx.sink
+                |> Dict.foldl
+                    (\to _ -> removeE { from = key, to = to })
+                |> (|>) vx.source
+                |> Iddict.remove key
+
+        Nothing ->
+            g
+
+
+toList : Graph vertex edge -> List ( Int, Vertex vertex edge )
+toList =
+    Iddict.toList
+
+
+sizeE : Graph vertex edge -> Int
+sizeE =
+    Iddict.foldl (\_ vx s -> s + Dict.size vx.source) 0
+
+
+sizeV : Graph vertex edge -> Int
+sizeV =
+    Iddict.size
